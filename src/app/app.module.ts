a3f2dbfc4f5339011f397dd6d44234d19ae01c56
Vika Moshura <vika.moshura@gmail.com>
2023-03-09 12:01:09 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters/characters.component';
+import { CharacterComponent } from './characters/character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
