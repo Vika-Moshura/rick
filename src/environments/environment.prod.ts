3a0a6c1b849d2bad9b81dcbc8f01b90f22c75682
Vika-Moshura <vika.moshura@gmail.com>
2023-03-18 19:34:53 +0200

    Environment.production updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
-    BACKEND_URL: 'https://rickandmortyapi.com/api',
-    production: true,
-  };
-  
+  BACKEND_URL: 'https://rickandmortyapi.com/api',
+  production: true,
+  auth: {
+    domain: 'dev-sexm5m3ibdykxiv6.us.auth0.com',
+    clientId: 'QP3XtIrRDNIMKpIpQxkbJxwyGukn9o0S',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    },
+  }
+}
